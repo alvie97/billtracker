04d3c2dd440132b4f5ba18bed17acabddfd23c2d
alvie97 <alvie97m@gmail.com>
2021-04-18 23:23:20 -0300

    initial configurations

---
+++ backend/src/main/java/com/billtracker/backend/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.billtracker.backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
